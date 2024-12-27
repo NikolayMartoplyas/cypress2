6c4c6d6aeebccc43650ff8a90d7a6fc10fefaf32
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-12-27 23:06:29 +0300

    Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '94tnyv',
   e2e: {
+    retries: 2,
     baseUrl: "https://qamid.tmweb.ru/", 
     setupNodeEvents(on, config) {
       // implement node event listeners here
